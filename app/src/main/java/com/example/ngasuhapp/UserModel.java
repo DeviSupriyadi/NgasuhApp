d28a09355c954fda0567589946ec2b4e00dd9dec
devisupriyadi <devisupriyadi0310@gmail.com>
2020-04-29 16:52:22 +0700

    latihan

--- app/src/main/java/com/example/ngasuhapp/UserModel.java
+++ app/src/main/java/com/example/ngasuhapp/UserModel.java
@@ -11,22 +11,27 @@ public class UserModel extends AppCompatActivity {
     private String phone;
 
     public String getUsername() {
+
         return username;
     }
 
     public void setUsername(String username) {
+
         this.username = username;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
     public String getPhone() {
+
         return phone;
     }
 
--- app/src/main/java/latihansharedpreference/RegisterActivity.java
+++ app/src/main/java/latihansharedpreference/RegisterActivity.java
@@ -1,4 +1,22 @@
 package latihansharedpreference;
 
+import android.content.Intent;
+
+import com.example.ngasuhapp.UserModel;
+
+import latihansharedpreferences.utils.Preferences;
+
 public class RegisterActivity {
+
+    UserModel userModel = new UserModel();
+    userModel.setUsername(userName);
+    userModel.setPassword(password);
+    userModel.setPhone(phoneNumber);
+
+    Preferences.setUserPreferences(getBaseContext(), userModel);
+    Preferences.setLoggedInStatus(getBaseContext(), true);
+
+    startActivity(new Intent(getBaseContext(), HomeActivity,class));
+    finish();
+
 }
